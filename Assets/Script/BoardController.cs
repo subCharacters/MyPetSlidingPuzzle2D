fdf50c23dc0cd9e1ebed2def23064b28ac39b83f
subCharacters <wldns59@naver.com>
2025-08-27 23:11:07 +0900

    First commit - Unity project setup

---
+++ Assets/Script/BoardController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BoardController : MonoBehaviour
+{
+    private GridLayoutGroup _grid;
+
+    private void Awake()
+    {
+        _grid = GetComponent<GridLayoutGroup>();
+        Debug.Log("[BoardController] Awake - 보드 준비 완료");
+    }
+
+    private void Start()
+    {
+        Debug.Log("[BoardController] Start - 보드 시작됨");
+        // 다음 단계: 여기서 퍼즐 타일을 생성할 예정
+    }
+}
---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log("[GameController] Awake - 씬 준비 시작");
+    }
+
+    private void Start()
+    {
+        Debug.Log("[GameController] Start - 초기화 완료");
+        // 앞으로 여기서 퍼즐 보드를 불러올 예정
+    }
+}
